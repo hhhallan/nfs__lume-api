be0eb53eeed63ada5f269dd1f7a2f2b117395952
Allan Leblond <74179820+hhhallan@users.noreply.github.com>
2023-06-26 15:12:30 +0200

    Mise en place + CRUD et auth User (#6)
    * User/auth (#4)
    
    * Connexion à la base de données
    
    * Création du modèle User + validation des champs
    
    * Lancement de l'API
    
    * Architecture
    
    * User - Route getAll, getOneById, create
    
    * User - CRUD
    
    * Refacto User
    
    * JWT - en cours
    
    * Ajout du token lors de la connexion
    
    * Middleware Authentification
    
    * Gestion des tokens différentes: tableaux de tokens
    
    * User - Login + Logout
    
    * Reafcto USER + route login / logout / create (#3)
    
    * CORS (#5)

---
+++ app.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const sequelize = require('./src/services/sequelize');
+const cors = require('cors');
+
+// Models
+const User = require('./src/models/user');
+
+// Lancement
+const express = require('express');
+const app = express();
+app.use(cors());
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Routes
+const userRoutes = require('./src/routes/user');
+app.use('/users', userRoutes);
+
+
+
+// Lancement
+app.listen(port, () => {
+    console.log(`Serveur lancé sur: http://localhost:${port}`)
+});
