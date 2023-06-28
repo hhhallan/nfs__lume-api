23497b7612b5c918c38d4e778cfc60bd59f37828
hhhallan <allanstm76@gmail.com>
2023-06-28 11:10:40 +0200

    Users & Admin

--- src/routes/scooter.js
+++ src/routes/scooter.js
@@ -3,11 +3,12 @@ const router = new express.Router();
 const isAuthenticated = require('../middlewares/auth');
 const scooterController = require('../controllers/scooter');
 
+// Users
 router.get('/available', scooterController.available);
-router.post('/:id/rent', isAuthenticated, scooterController.rent);
-router.post('/:id/stop-rent', isAuthenticated, scooterController.stopRent);
+// router.post('/:id/rent', isAuthenticated, scooterController.rent);
+// router.post('/:id/stop-rent', isAuthenticated, scooterController.stopRent);
 
-
+// Admin
 router.get('/', scooterController.getAll);
 router.get('/:id', scooterController.getOneById);
 router.post('/', scooterController.create);
