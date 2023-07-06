c2cbca1ccbbfd66c8a903ea4da13f44dc5aff488
hhhallan <allanstm76@gmail.com>
2023-07-06 16:16:08 +0200

    Jvais push

--- src/services/sequelize.js
+++ src/services/sequelize.js
@@ -2,11 +2,12 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize({
-    database: 'lume_api',
-    username: 'root',
-    password: '',
-    host: 'localhost',
+    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
+    port: process.env.DB_PORT
 });
 
 module.exports = sequelize;
